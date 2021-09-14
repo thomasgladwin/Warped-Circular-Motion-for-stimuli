da9a2a504e8b5f4606ea503356df2a17c7e806bd
Thomas Edward Gladwin <thomas.gladwin@gmail.com>
2021-09-14 23:19:37 +0100

    Add files via upload

--- geomaker_anim.R
+++ geomaker_anim.R
@@ -12,16 +12,27 @@ geomaker <- function(savename, angle_offset, nPoints, xrparams, yrparams) {
   p0 <- ggplot(D, aes(x = x, y = y)) + geom_polygon(fill="white", colour="black", size=5)
   p0 <- p0 + theme_void() + theme(legend.position="none") + theme(aspect.ratio=1)
   if (savename != '') {
-    ggsave(savename, plot = p0)
+    ggsave(savename, plot = p0, dpi=72)
   }
   print(p0)
+}
+
+zstr0 <- function(val, len0) {
+  str0 <- ''
+  for (pow0 in 1:len0) {
+    valmod <- val %% 10;
+    val <- floor(val / 10);
+    str0 <- paste0(valmod, str0)
+  }
+  return(str0)
 }
 
 minNPoints <- 199
 maxNPoints <- 200
 nPoses <- 2 + 1
 nSteps <- 200
-maxF <- 12
+maxF1 <- 8
+maxF2 <- 8
 
 offset_00 <- 0
 nPoints_00 <- 4
@@ -45,8 +56,8 @@ for (iPose in 1:nPoses) {
     if (nPoints_2 == 8) {
       nPoints_2 = 200
     }
-    xrparams_2 <- runif(4) * c(1, 1, maxF, 1)
-    yrparams_2 <- runif(4) * c(1, 1, maxF, 1)
+    xrparams_2 <- runif(4) * c(1, 1, maxF1, 1)
+    yrparams_2 <- runif(4) * c(1, 1, maxF2, 1)
   }
   for (iStep in 1:nSteps) {
     prop0 <- (iStep - 1) / nSteps
@@ -54,7 +65,7 @@ for (iPose in 1:nPoses) {
     nPoints_0 <- floor(nPoints_1 + (nPoints_2 - nPoints_1) * prop0);
     xrparams_0 <- xrparams_1 + (xrparams_2 - xrparams_1) * prop0;
     yrparams_0 <- yrparams_1 + (yrparams_2 - yrparams_1) * prop0;
-    geomaker(paste0('anim2_', iFrame, '.png'), offset_0, nPoints_0, xrparams_0, yrparams_0)
+    geomaker(paste0('anim3_', zstr0(iFrame, 8), '.png'), offset_0, nPoints_0, xrparams_0, yrparams_0)
     iFrame <- iFrame + 1
   }
   offset_1 <- offset_2
